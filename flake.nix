dc9e57f608550d304687245dd9f6f523ec6871ce
H1rono <hronok66@gmail.com>
2024-04-04 01:27:41 +0900

    :tada: Init Nix Flakes

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "git講習会 スライド";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        inherit (pkgs) stdenv lib;
+        html = stdenv.mkDerivation {
+          pname = "git-lecture-slide";
+          version = "0.1.0";
+          src = ./.;
+          nativeBuildInputs = with pkgs; [ marp-cli ];
+          buildPhase = ''
+            mkdir -p out
+            marp -o out/index.html slide.md
+          '';
+          installPhase = ''
+            mkdir -p $out
+            mv out $out/slide
+          '';
+        };
+      in
+      {
+        packages = {
+          default = html;
+          inherit html;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ marp-cli ];
+        };
+      }
+    );
+}
